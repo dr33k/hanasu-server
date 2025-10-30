a92b9ca53dc8dde22e32d5366e10d5729142066a
EmelieC <emeliec@hardcorebiometric.com>
2025-10-30 17:49:49 +0100

    Initial commit

---
+++ src/main/java/com/seven/hanasu/HanasuApplication.java
@@ -0,0 +1,13 @@
+package com.seven.hanasu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanasuApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hanasu");SpringApplication.run(HanasuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seven/hanasu/HanasuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seven.hanasu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanasuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
